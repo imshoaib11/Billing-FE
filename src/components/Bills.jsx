415f25e4b2d8d01a889772272f84fe12775451ea
imshoaib11 <160542341+imshoaib11@users.noreply.github.com>
2024-10-16 22:01:22 +0530

    Update Bills.jsx

--- src/components/Bills.jsx
+++ src/components/Bills.jsx
@@ -125,9 +125,9 @@ export function NewBills(){
                 <div className="mb-3">
                 <button  style={{width:"30%"}}className="btn btn-success mt-3" onClick={()=>sendPayment()}>PAY</button>
                 </div>
-            </form>
-            </div>
             
+            </div>
+            </form>
            
             
        </div>
